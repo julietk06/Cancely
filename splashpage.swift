3d62cfdf641869b6b57f5ac740f991f9d871f668
julietk06 <35513264+julietk06@users.noreply.github.com>
2020-04-30 20:59:51 -0400

    Update splashpage.swift

--- splashpage.swift
+++ splashpage.swift
@@ -1,1 +1,117 @@
+// ColorScheme
 
+
+var view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 375, height: 442)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+//splash page
+let style = UILabel(frame: CGRect(x: 0, y: 0, width: 375, height: 812))
+style.clipsToBounds = true
+style.backgroundColor = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1)
+style.alpha = 1
+
+
+let style = UILabel(frame: CGRect(x: 121, y: 798, width: 134, height: 5))
+style.clipsToBounds = true
+style.layer.cornerRadius = 100
+style.backgroundColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1)
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 0, y: 778, width: 375, height: 34))
+style.clipsToBounds = true
+style.alpha = 1
+
+
+let style = UILabel(frame: CGRect(x: 0, y: 0, width: 375, height: 44))
+style.clipsToBounds = true
+style.alpha = 1 = true
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 0, y: 0, width: 375, height: 44))
+style.clipsToBounds = true
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 21, y: 12, width: 54, height: 21))
+style.clipsToBounds = true
+style.alpha = 1
+// Text style for "9:41"
+let style = UILabel(frame: CGRect(x: 21, y: 14, width: 54, height: 18))
+style.clipsToBounds = true
+style.alpha = 1
+style.text = "9:41"
+style.font = UIFont(name: "MuseoSansRounded-500", size: 15)
+style.textColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1)
+style.lineSpacing = -0.3
+let style = UILabel(frame: CGRect(x: 21, y: 12, width: 54, height: 21))
+style.clipsToBounds = true
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 293, y: 17, width: 18, height: 12))
+style.clipsToBounds = true
+style.backgroundColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1)
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 315, y: 17, width: 16, height: 12))
+style.clipsToBounds = true
+style.backgroundColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1)
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 336, y: 17, width: 25, height: 12))
+style.clipsToBounds = true
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 338, y: 19, width: 18, height: 8))
+style.clipsToBounds = true
+style.layer.cornerRadius = 1
+style.backgroundColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1)
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 359, y: 21, width: 2, height: 4))
+style.clipsToBounds = true
+style.backgroundColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1)
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 336, y: 17, width: 22, height: 12))
+style.clipsToBounds = true
+style.layer.cornerRadius = 3
+style.layer.borderColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1).cgColor
+style.layer.borderWidth = 1
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 0, y: 0, width: 375, height: 44))
+style.clipsToBounds = true
+style.alpha = 1
+// Text style for "Cancely"
+let style = UILabel(frame: CGRect(x: 101, y: 378, width: 172, height: 55))
+style.clipsToBounds = true
+style.alpha = 1
+style.text = "Cancely"
+style.font = UIFont(name: "MuseoSansRounded-900", size: 44)
+style.textColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1)
+let style = UILabel(frame: CGRect(x: 76, y: 707, width: 223, height: 19))
+style.clipsToBounds = true
+style.alpha = 1
+// Text style for "70%"
+let style = UILabel(frame: CGRect(x: 76, y: 707, width: 27, height: 17))
+style.clipsToBounds = true
+style.alpha = 1
+style.text = "70%"
+style.font = UIFont(name: "MuseoSansRounded-500", size: 14)
+style.textColor = UIColor(red: 0.35, green: 0.81, blue: 0.88, alpha: 1)
+let style = UILabel(frame: CGRect(x: 76, y: 724, width: 161, height: 2))
+style.clipsToBounds = true
+style.layer.borderColor = UIColor(red: 0.35, green: 0.81, blue: 0.88, alpha: 1).cgColor
+style.layer.borderWidth = 2
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 76, y: 724, width: 223, height: 2))
+style.clipsToBounds = true
+style.layer.borderColor = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1).cgColor
+style.layer.borderWidth = 2
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 112, y: 799, width: 150, height: 6))
+style.clipsToBounds = true
+style.layer.borderColor = UIColor(red: 0.87, green: 0.87, blue: 0.87, alpha: 1).cgColor
+style.layer.borderWidth = 5
+style.alpha = 1
+let style = UILabel(frame: CGRect(x: 0, y: 0, width: 375, height: 812))
+style.clipsToBounds = true
+style.backgroundColor = UIColor(red: 0.35, green: 0.81, blue: 0.88, alpha: 1)
+style.alpha = 1
